bec015671cade0edcf183ebe46461381ae45d20a
Kaarthikcharan <84065849+Kaarthikcharan@users.noreply.github.com>
2022-07-02 18:05:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+video="";
+function preload(){
+    video=createVideo("video.mp4")
+    video.hide();
+}
+function setup(){
+    canvas=createCanvas(480, 380);
+    canvas.center();
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status!=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status: object detected";
+            document.getElementById("number_of_objects").innerHTML="Number of objects detected are: "+objects.length;
+            fill("#2cf60a");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#2cf60a");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML="Status : Detecting objects";    
+}
